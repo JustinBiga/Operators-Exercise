99dee6517c2792da85f81492fb7db009f7f87cfe
JustinBiga <BISENGOJ@GMAIL.COM>
2024-03-26 14:52:22 -0400

    Exercise 1 complete

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            //Addition
+
+            int sum = a + b;
+            Console.WriteLine($"{a} + {b} is equal to {sum}");
+
+            //Subtraction
+
+            int sub = a - b;
+
+            Console.WriteLine($"{a}-{b} is equal to {sub}");
+
+            //multiplication
+
+            int multiply = a * b;
+
+            Console.WriteLine($"{a} * {b} is equal to {multiply}");
+
+            //Division
+
+            int divide = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {divide} remainder {remainder}");
+
+
+
+
+
         }
     }
 }
